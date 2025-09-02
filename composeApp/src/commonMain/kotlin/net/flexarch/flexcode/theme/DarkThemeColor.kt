15ea5a268eab83f968ad1307fe151b861e80005b
reyhanrammadhan09 <reyhanramadhan9928@gmail.com>
2025-09-02 19:46:24 +0700

    first set up

---
+++ composeApp/src/commonMain/kotlin/net/flexarch/flexcode/theme/DarkThemeColor.kt
@@ -0,0 +1,18 @@
+package net.flexarch.flexcode.theme
+
+// Dark Theme Color
+const val primaryColor = 0xFF
+const val primaryColor_V1 = 0xFF
+const val surfaceColor = 0xFF252525
+const val surfaceColor_V1 = 0xFF202020
+const val surfaceColor_V2 = 0xFF1B1B1B
+const val surfaceContainerColor = 0xFF252525
+const val backgroudColor = 0xFF252525
+const val errorColor = 0xFFB00020
+
+// on color
+const val onPrimaryColor = 0xFFFFFFFF
+const val onSurfaceColor = 0xFFFFFFFF
+const val onSurfaceContainerColor = 0xFFFFFFFF
+const val onBackgroudColor = 0xFFFFFFFF
+const val onErrorColor = 0xFFFFFFFF
